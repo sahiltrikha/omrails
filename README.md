2a157552e3c6df59bffa7a16dba486ad78234e14
Sahil Trikha <trikha93@gmail.com>
2013-06-24 17:48:11 -0400

    testing

---
+++ README.md
@@ -0,0 +1,4 @@
+#One Month Rails
+
+sample app for 
+[*One Month Rails*](http://onemonthrails.com)
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Omrails::Application.config.secret_token = 'a0ddc88a4600bfef1dc15a3fef234f74ea03c0a51c5391022a772c90c4651a3bbec88cb5cbc8d82819bbf4aa6fea3f7b827e86a2355c7c57d45820545f86940e'
